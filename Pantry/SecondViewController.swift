4e313651c4d9a82ce623022bf7fe18acb86ced4f
erdannenbaum <erdannenbaum@gmail.com>
2018-05-02 09:34:36 -0400

    Initial Commit

---
+++ Pantry/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Pantry
+//
+//  Created by Eric Dannenbaum on 5/2/18.
+//  Copyright © 2018 Eric Dannenbaum. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
